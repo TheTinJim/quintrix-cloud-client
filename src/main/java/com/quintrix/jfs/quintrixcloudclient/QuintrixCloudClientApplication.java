12dedc79e050594e27881a4c0b53abcd53c85d08
Brandon Northrop <brannorth0414@yahoo.com>
2022-06-05 10:41:40 -0600

    Initial commit

---
+++ src/main/java/com/quintrix/jfs/quintrixcloudclient/QuintrixCloudClientApplication.java
@@ -0,0 +1,13 @@
+package com.quintrix.jfs.quintrixcloudclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuintrixCloudClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuintrixCloudClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quintrix/jfs/quintrixcloudclient/QuintrixCloudClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quintrix.jfs.quintrixcloudclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuintrixCloudClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
